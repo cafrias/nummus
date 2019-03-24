796d97fdef4f7ab9cf94520aeceb3c8f6b63b087
friasdesign <carlos.a.frias@gmail.com>
2019-03-24 12:56:10 -0300

    Fix apollo vscode bug

--- apollo.config.js
+++ apollo.config.js
@@ -2,13 +2,7 @@ module.exports = {
   client: {
     service: {
       name: "nummus",
-      // endpoint: {
-      //   url: "http://localhost:4000"
-      // },
-      // OR
-      localSchemaFile: './src/schema.graphql',
-      includes: ['src/**/*.{graphql,ts,js,tsx}'],
-      excludes: ['**/node_modules/**'],
-    }
-  }
-};
+      localSchemaFile: "schema/schema.graphql",
+    },
+  },
+}
