2655430fbf5ba000bc38f55d33221f1eb6d2534c
Carlos Frias <carlos.a.frias@gmail.com>
2019-03-27 11:40:11 -0300

    Refactor Transaction form

--- apps/web-client/src/components/Transaction/Forms/Create.tsx
+++ apps/web-client/src/components/Transaction/Forms/Create.tsx
@@ -1,34 +1,20 @@
 import * as React from "react"
 
-import { Form, Formik, FormikErrors, Field, FieldProps } from "formik"
+import { FormikErrors } from "formik"
 
-import Button from "@material-ui/core/Button"
 import Grid from "@material-ui/core/Grid"
 import UIField from "~/components/UI/Field"
 import { CreateFormProps } from "~/components/UI/Forms/Create"
-
-import classNames from "classnames"
-
-import FormControlLabel from "@material-ui/core/FormControlLabel"
-import Switch from "@material-ui/core/Switch"
-import InputAdornment from "@material-ui/core/InputAdornment"
-import green from "@material-ui/core/colors/green"
-import deepOrange from "@material-ui/core/colors/deepOrange"
-
-import Icon from "@material-ui/core/Icon"
-import AddIcon from "@material-ui/icons/Add"
-import RemoveIcon from "@material-ui/icons/Remove"
-
-import { makeStyles, createStyles } from "@material-ui/styles"
 import { IdName } from "~/types/IdLabel"
 import { SpendGroup } from "@nummus/schema"
+import RecordFormsCreate, {
+  RecordFormsCreateValues,
+} from "~/components/Record/Forms/Create"
 
 // ---------------------------------------------------------------------------------------------------------------------
 // Types
 // ---------------------------------------------------------------------------------------------------------------------
-export interface TransactionFormsCreateValues {
-  amount: number
-  incoming: boolean
+export interface TransactionFormsCreateValues extends RecordFormsCreateValues {
   category: string
 }
 
@@ -45,110 +31,38 @@ const groupLabels = {
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
-// Styles
-// ---------------------------------------------------------------------------------------------------------------------
-const useStyles = makeStyles(() =>
-  createStyles({
-    amountAdornment: {
-      color: deepOrange[500],
-    },
-    incoming: {
-      color: green[500],
-    },
-  })
-)
-
-// ---------------------------------------------------------------------------------------------------------------------
 // Component
 // ---------------------------------------------------------------------------------------------------------------------
 const TransactionFormsCreate: React.SFC<
   TransactionFormsCreateProps & CreateFormProps<TransactionFormsCreateValues>
-> = ({ onSubmit, categories }) => {
-  const classes = useStyles()
-
+> = props => {
   return (
-    <Formik
-      initialValues={{
-        amount: 0,
-        incoming: false,
-        category: "",
-      }}
-      onSubmit={onSubmit}
-      validate={values => {
+    <RecordFormsCreate
+      accountId={props.accountId}
+      submitTestId="transaction_create"
+      initialValues={{ category: "" }}
+      validate={(values: TransactionFormsCreateValues) => {
         const errors: FormikErrors<TransactionFormsCreateValues> = {}
-        if (values.amount <= 0) errors.amount = "Should be greater than 0"
         if (!values.category) errors.category = "Select a category"
         return errors
       }}
+      onSubmit={props.onSubmit}
     >
-      {form => {
-        const { incoming } = form.values
-        return (
-          <Form>
-            <Grid container justify="flex-end" spacing={16}>
-              <Grid item xs={12} md={6}>
-                <Field name="incoming">
-                  {({ field }: FieldProps) => (
-                    <FormControlLabel
-                      control={<Switch {...field} checked={field.value} />}
-                      label="Incoming"
-                    />
-                  )}
-                </Field>
-              </Grid>
-              <Grid item xs={12} md={6}>
-                <UIField
-                  name="amount"
-                  TextFieldProps={{
-                    id: "amount",
-                    label: "Amount",
-                    type: "number",
-                    InputProps: {
-                      startAdornment: (
-                        <InputAdornment disableTypography position="start">
-                          <Icon
-                            className={classNames(classes.amountAdornment, {
-                              [classes.incoming]: incoming,
-                            })}
-                          >
-                            {incoming ? <AddIcon /> : <RemoveIcon />}
-                          </Icon>
-                        </InputAdornment>
-                      ),
-                    },
-                  }}
-                />
-              </Grid>
-              <Grid item xs={12} md={6}>
-                <UIField
-                  name="category"
-                  TextFieldProps={{
-                    id: "category",
-                    label: "Category",
-                    select: true,
-                    children: [
-                      <option key={0} value="" />,
-                      renderCategories(categories),
-                    ],
-                  }}
-                />
-              </Grid>
-              <Grid item xs={12} md={4}>
-                <Button
-                  color="primary"
-                  variant="contained"
-                  type="submit"
-                  fullWidth
-                  data-testid="transaction_create"
-                >
-                  Create
-                </Button>
-              </Grid>
-            </Grid>
-          </Form>
-        )
-      }}
-    </Formik>
+      <Grid item xs={12} md={6}>
+        <UIField
+          name="category"
+          TextFieldProps={{
+            id: "category",
+            label: "Category",
+            select: true,
+            children: [
+              <option key={0} value="" />,
+              renderCategories(props.categories),
+            ],
+          }}
+        />
+      </Grid>
+    </RecordFormsCreate>
   )
 }
 
