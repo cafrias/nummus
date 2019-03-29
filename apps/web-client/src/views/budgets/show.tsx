b34a51be253b80d5ff2998b2771bd83e104f19e4
Carlos Frias <carlos.a.frias@gmail.com>
2019-03-29 16:24:10 -0300

    Add basic phone layout

--- apps/web-client/src/views/budgets/show.tsx
+++ apps/web-client/src/views/budgets/show.tsx
@@ -1,8 +1,26 @@
 import * as React from "react"
 import { ViewProps } from "../Root"
 
+import classNames from "classnames"
+
+import Chip from "@material-ui/core/Chip"
+import ExpansionPanel from "@material-ui/core/ExpansionPanel"
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary"
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails"
+import Table from "@material-ui/core/Table"
+import TableBody from "@material-ui/core/TableBody"
+import TableCell from "@material-ui/core/TableCell"
+import TableHead from "@material-ui/core/TableHead"
+import TableRow from "@material-ui/core/TableRow"
+import Typography from "@material-ui/core/Typography"
 import Fab from "@material-ui/core/Fab"
 import AddIcon from "@material-ui/icons/Add"
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
+
+import green from "@material-ui/core/colors/green"
+import grey from "@material-ui/core/colors/grey"
+import deepOrange from "@material-ui/core/colors/deepOrange"
+import red from "@material-ui/core/colors/red"
 
 import { makeStyles, createStyles } from "@material-ui/styles"
 import { Theme } from "@material-ui/core/styles"
@@ -17,6 +35,28 @@ const useStyles = makeStyles((t: Theme) =>
       position: "fixed",
       bottom: `${t.spacing.unit * 2}px`,
       right: `${t.spacing.unit * 2}px`,
+    },
+    hideXS: {
+      [t.breakpoints.down("sm")]: {
+        display: "none",
+      },
+    },
+    available: {
+      width: "100%",
+      fontWeight: "bold",
+      fontSize: "1.05rem",
+    },
+    surplus: {
+      backgroundColor: green[600],
+      color: t.palette.getContrastText(green[600]),
+    },
+    deficit: {
+      backgroundColor: red[400],
+      color: t.palette.getContrastText(red[400]),
+    },
+    check: {
+      backgroundColor: grey[200],
+      color: t.palette.getContrastText(grey[200]),
     },
   })
 )
@@ -36,6 +76,76 @@ const BudgetsShow: React.SFC<BudgetsShowProps> = props => {
 
   return (
     <main>
+      <ExpansionPanel defaultExpanded={true}>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography component="h2" variant="h6">
+            Immediate Obligations
+          </Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Category</TableCell>
+                <TableCell align="right" className={classes.hideXS}>
+                  Budgeted
+                </TableCell>
+                <TableCell align="right" className={classes.hideXS}>
+                  Spent
+                </TableCell>
+                <TableCell align="center">Available</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>Internet</TableCell>
+                <TableCell align="right" className={classes.hideXS}>
+                  250
+                </TableCell>
+                <TableCell align="right" className={classes.hideXS}>
+                  300
+                </TableCell>
+                <TableCell>
+                  <Chip
+                    className={classNames(classes.available, classes.deficit)}
+                    label="-50"
+                  />
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Gas</TableCell>
+                <TableCell align="right" className={classes.hideXS}>
+                  800
+                </TableCell>
+                <TableCell align="right" className={classes.hideXS}>
+                  0
+                </TableCell>
+                <TableCell>
+                  <Chip
+                    className={classNames(classes.available, classes.surplus)}
+                    label="800"
+                  />
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Rent</TableCell>
+                <TableCell align="right" className={classes.hideXS}>
+                  1200
+                </TableCell>
+                <TableCell align="right" className={classes.hideXS}>
+                  1200
+                </TableCell>
+                <TableCell>
+                  <Chip
+                    className={classNames(classes.available, classes.check)}
+                    label="0"
+                  />
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
       <Fab
         className={classes.fab}
         color="primary"
