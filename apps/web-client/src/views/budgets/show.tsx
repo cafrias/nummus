3ef6a8eaee62898784b6bd016c1f4590934e6e32
Carlos Frias <carlos.a.frias@gmail.com>
2019-04-03 09:29:38 -0300

    Add layout

--- apps/web-client/src/views/budgets/show.tsx
+++ apps/web-client/src/views/budgets/show.tsx
@@ -4,6 +4,7 @@ import { ViewProps } from "../Root"
 import classNames from "classnames"
 
 import IconButton from "@material-ui/core/IconButton"
+import Grid from "@material-ui/core/Grid"
 import Chip from "@material-ui/core/Chip"
 import ExpansionPanel from "@material-ui/core/ExpansionPanel"
 import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary"
@@ -16,6 +17,7 @@ import TableRow from "@material-ui/core/TableRow"
 import TableFooter from "@material-ui/core/TableFooter"
 import Typography from "@material-ui/core/Typography"
 import Fab from "@material-ui/core/Fab"
+import Paper from "@material-ui/core/Paper"
 
 import AddIcon from "@material-ui/icons/Add"
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
@@ -37,6 +39,21 @@ const useStyles = makeStyles((t: Theme) =>
       position: "fixed",
       bottom: `${t.spacing.unit * 2}px`,
       right: `${t.spacing.unit * 2}px`,
+      zIndex: 10,
+    },
+    categoriesContainer: {
+      paddingTop: `${t.spacing.unit * 8}px`,
+    },
+    toBeBudgeted: {
+      position: "fixed",
+      top: 0,
+      left: 0,
+      width: "100%",
+      padding: `${t.spacing.unit}px`,
+      zIndex: 9,
+    },
+    panelDetails: {
+      padding: 0,
     },
     hideXS: {
       [t.breakpoints.down("sm")]: {
@@ -78,88 +95,91 @@ const BudgetsShow: React.SFC<BudgetsShowProps> = props => {
 
   return (
     <main>
-      <ExpansionPanel defaultExpanded={true}>
-        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography component="h2" variant="h6">
-            Immediate Obligations
-          </Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Category</TableCell>
-                <TableCell align="right" className={classes.hideXS}>
-                  Budgeted
-                </TableCell>
-                <TableCell align="right" className={classes.hideXS}>
-                  Spent
-                </TableCell>
-                <TableCell align="center">Available</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableRow>
-                <TableCell>Internet</TableCell>
-                <TableCell align="right" className={classes.hideXS}>
-                  250
-                </TableCell>
-                <TableCell align="right" className={classes.hideXS}>
-                  300
-                </TableCell>
-                <TableCell>
-                  <Chip
-                    className={classNames(classes.available, classes.deficit)}
-                    label="-50"
-                  />
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>Gas</TableCell>
-                <TableCell align="right" className={classes.hideXS}>
-                  800
-                </TableCell>
-                <TableCell align="right" className={classes.hideXS}>
-                  0
-                </TableCell>
-                <TableCell>
-                  <Chip
-                    className={classNames(classes.available, classes.surplus)}
-                    label="800"
-                  />
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>Rent</TableCell>
-                <TableCell align="right" className={classes.hideXS}>
-                  1200
-                </TableCell>
-                <TableCell align="right" className={classes.hideXS}>
-                  1200
-                </TableCell>
-                <TableCell>
-                  <Chip
-                    className={classNames(classes.available, classes.check)}
-                    label="0"
-                  />
-                </TableCell>
-              </TableRow>
-            </TableBody>
-            <TableFooter>
-              <TableRow>
-                <TableCell />
-                <TableCell className={classes.hideXS} />
-                <TableCell className={classes.hideXS} />
-                <TableCell align="center">
-                  <IconButton color="default">
-                    <AddIcon />
-                  </IconButton>
-                </TableCell>
-              </TableRow>
-            </TableFooter>
-          </Table>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
+      <Paper className={classes.toBeBudgeted} square>
+        <Grid container spacing={8} alignItems="center">
+          <Grid item xs={6}>
+            <Typography variant="subtitle1">To be budgeted</Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Chip
+              className={classNames(classes.available, classes.check)}
+              label="0"
+            />
+          </Grid>
+        </Grid>
+      </Paper>
+      <div className={classes.categoriesContainer}>
+        <ExpansionPanel defaultExpanded={true}>
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography component="h2" variant="h6">
+              Immediate Obligations
+            </Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails className={classes.panelDetails}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Category</TableCell>
+                  <TableCell align="right" className={classes.hideXS}>
+                    Budgeted
+                  </TableCell>
+                  <TableCell align="right" className={classes.hideXS}>
+                    Spent
+                  </TableCell>
+                  <TableCell align="center">Available</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell>Internet</TableCell>
+                  <TableCell align="right" className={classes.hideXS}>
+                    250
+                  </TableCell>
+                  <TableCell align="right" className={classes.hideXS}>
+                    300
+                  </TableCell>
+                  <TableCell>
+                    <Chip
+                      className={classNames(classes.available, classes.deficit)}
+                      label="-50"
+                    />
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Gas</TableCell>
+                  <TableCell align="right" className={classes.hideXS}>
+                    800
+                  </TableCell>
+                  <TableCell align="right" className={classes.hideXS}>
+                    0
+                  </TableCell>
+                  <TableCell>
+                    <Chip
+                      className={classNames(classes.available, classes.surplus)}
+                      label="800"
+                    />
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Rent</TableCell>
+                  <TableCell align="right" className={classes.hideXS}>
+                    1200
+                  </TableCell>
+                  <TableCell align="right" className={classes.hideXS}>
+                    1200
+                  </TableCell>
+                  <TableCell>
+                    <Chip
+                      className={classNames(classes.available, classes.check)}
+                      label="0"
+                    />
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </div>
       <Fab
         className={classes.fab}
         color="primary"
